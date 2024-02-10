3c1c8c1233a7b1415292d536f6fb9310ece05aec
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2024-02-10 02:29:43 +0000

    ejercicios

--- exercises/03.2-a-real-component/app.jsx
+++ exercises/03.2-a-real-component/app.jsx
@@ -3,5 +3,25 @@ import ReactDOM from "react-dom";
 
 // Create your function here
 
-// Remember to use ReactDOM.render to include the component into the website
+const BootstrapCard = () => {
+	return (
+		<div className="card m-5">
+			<img className="card-img-top" src="../../.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
+			<div className="card-body">
+				<h5 clasNameName="card-title">Bob Dylan</h5>
+				<p className="card-text">
+					Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an
+					influential figure in popular music and culture for more than five decades.
+				</p>
+				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
+					Go to wikipedia
+				</a>
+			</div>
+		</div>
+	);
+};
 
+// Change the syntax of the first parameter to make it <PrintHello /> instead of PrintHello()
+ReactDOM.render(<BootstrapCard />, document.querySelector("#myDiv"));
+
+// Remember to use ReactDOM.render to include the component into the website
